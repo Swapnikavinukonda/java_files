26479f5d17736079269a365b9304eae008cdcf5d
Swapnikavinukonda <149222290+Swapnikavinukonda@users.noreply.github.com>
2024-03-21 12:42:01 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class add_1 
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter values of a and b");
+int a=sc.nextInt();
+int b=sc.nextInt();
+System.out.println(a+"+"+b+"is" +(a+b));
+System.out.println(a+"-"+b+"is" +(a-b));
+System.out.println(a+"*"+b+"is" +(a*b));
+System.out.println(a+"/"+b+"is" +(a/b));
+}
+}
