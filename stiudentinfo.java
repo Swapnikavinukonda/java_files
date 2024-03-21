d2c8bcbf0ebe1c4ddc9eb10da8d2e3c6a0ec99ae
Swapnikavinukonda <149222290+Swapnikavinukonda@users.noreply.github.com>
2024-03-21 13:09:30 +0530

    Add files via upload

---
+++ stiudentinfo.java
@@ -0,0 +1,23 @@
+public class stiudentinfo
+{
+public static void main(String[]args)
+{
+Student s=new Studenr("swapnikia","7117");
+s.display();
+}
+}
+class Student              
+{
+string nam,rno;
+Student(String name,String rno)
+{
+nam=name;
+rn=rno;
+}
+public void display()
+{
+System.out,println("welcome %s rno is % \n"nam,rn)
+}
+}
+}
+
