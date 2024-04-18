74acb6c734faaa86aefd204467b7caeca69ee73f
Swapnikavinukonda <149222290+Swapnikavinukonda@users.noreply.github.com>
2024-04-18 11:58:36 +0530

    string comparision-18/04

---
+++ strcomparision.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class strcomparision
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter your subject");
+String subj=sc.next();
+String sch=new String("python");
+if(subj.equals("java"))
+{
+System.out.println("patience is needed");
+}
+else {
+System.out.println("dont waste time");
+}
+}
+}
