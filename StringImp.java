9c85bc0f19d41b048f2b9409bf7173533f92273a
Swapnikavinukonda <149222290+Swapnikavinukonda@users.noreply.github.com>
2024-04-18 11:43:17 +0530

    Add files via upload

---
+++ StringImp.java
@@ -0,0 +1,10 @@
+class StringImp
+{
+public static void main(String args[])
+{
+String str1="hello";
+String str2=new String(str1);
+System.out.println("string1 is :"+str1);
+System.out.println("string2 is :"+str2);
+}
+}
