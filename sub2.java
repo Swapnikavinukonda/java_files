2a0be843199ecddd217dd73e8ff575c79ff9cb03
Swapnikavinukonda <149222290+Swapnikavinukonda@users.noreply.github.com>
2024-04-18 13:12:20 +0530

    substraction_method2

---
+++ sub2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class sub2
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter values of a and b:");
+int a=sc.nextInt();
+int b=sc.nextInt();
+System.out.println("subtraction of "+a+" - "+b+" is "+(a-b));
+}
+}
