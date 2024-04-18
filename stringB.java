52a886e25b6ade91257b1a62360978a2e4096e1d
Swapnikavinukonda <149222290+Swapnikavinukonda@users.noreply.github.com>
2024-04-18 15:47:19 +0530

    string_buffer class

---
+++ stringB.java
@@ -0,0 +1,10 @@
+class stringB
+{
+public static void main(String [] args)
+{
+StringBuffer sb=new StringBuffer("study");
+System.out.println(sb);
+sb.append("toningt");
+System.out.println(sb);
+}
+}
