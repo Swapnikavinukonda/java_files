c7316f2ba1648016344480c0ff48ba3f047d2c7a
Swapnikavinukonda <149222290+Swapnikavinukonda@users.noreply.github.com>
2024-04-18 13:10:46 +0530

    add_method1

---
+++ add_1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class ADD
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter values of a and b:");
+int a=sc.nextInt();
+int b=sc.nextInt();
+System.out.println("addition of "+a+" + "+b+" is "+(a+b));
+}
+}
