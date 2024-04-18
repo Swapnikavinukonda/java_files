7b4b1f653a0d751673ebfe936834c348b7c9ad66
Swapnikavinukonda <149222290+Swapnikavinukonda@users.noreply.github.com>
2024-04-18 13:04:22 +0530

    typecast-narrowing

---
+++ narrowcastex.java
@@ -0,0 +1,12 @@
+public class narrowcastex
+{
+public static void main(String[]args)
+{
+float marks=10.0,sub=15.0;
+int dmarks=(int)marks;
+int submarks=(int)sub;
+System.out.println(marks);
+System.out.println(dmarks);
+System.out.println(submarks);
+}
+}
