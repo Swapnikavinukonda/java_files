d9757d96b7ccbe319d70042d0a26cd5fa61e0c50
Swapnikavinukonda <149222290+Swapnikavinukonda@users.noreply.github.com>
2024-04-18 12:40:45 +0530

    string length-18/04

---
+++ strlenchar.java
@@ -0,0 +1,11 @@
+class strlenchar
+{
+public static void main(String []args)
+{
+String s1="swapnika";
+String s2="swathika";
+System.out.println(s1+s2);
+System.out.println(s1.charAt(1));
+System.out.println(s1.length());
+}
+}
