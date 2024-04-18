7b532f1dac0936fd64fe0030e55665680a9854db
Swapnikavinukonda <149222290+Swapnikavinukonda@users.noreply.github.com>
2024-04-18 13:02:56 +0530

    string index-18/04

---
+++ studyTonight.java
@@ -0,0 +1,12 @@
+public class studyTonight
+{
+public static void main(String [] args)
+{
+String str="Tonight";
+System.out.println(str.indexOf('u'));
+System.out.println(str.indexOf('t',3));
+String subString="Ton";
+System.out.println(str.indexOf(subString));
+System.out.println(str.indexOf(subString,7));
+}
+}
