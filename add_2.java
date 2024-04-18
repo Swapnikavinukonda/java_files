d25abf87a58d8a150cbb6312edc605deb65b7edd
Swapnikavinukonda <149222290+Swapnikavinukonda@users.noreply.github.com>
2024-04-18 13:11:04 +0530

    add_method2

---
+++ add_2.java
@@ -0,0 +1,10 @@
+class add_2
+{
+public static void main(String args[])
+{
+//System.out.println("display" +ar[1]);
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[2]);
+System.out .println("result is :"+(a+b));
+}
+}
